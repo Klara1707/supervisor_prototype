e1150400776c7632cc25712cb077ba39b1b49c4a
Klara <klara.vandenburg@yahoo.com>
2025-11-09 07:46:41 +0800

    main added

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import LogInPages from "./pages/LogInPages.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        children: [
+        { path: "/", element: <HomePage /> },
+        { path: "/login", element: <LogInPages /> },
+        ],
+    },
+    ]);
+
+    ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} basename="/supervisor_prototype/" />
+    </React.StrictMode>
+);
