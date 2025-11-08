4c0f5fe6b08033bcdcca8bd1ae686786c2ca68e7
Klara <klara.vandenburg@yahoo.com>
2025-11-09 07:57:44 +0800

    home page added

---
+++ src/pages/HomePages.jsx
@@ -0,0 +1,21 @@
+
+import "./HomePage.css";
+import HeroBar from "../components/HeroBar";
+import TabMenu from "../components/TabMenu";
+import NavBar from "../components/NavBar";
+
+function HomePage() {
+    return (
+        <>
+        <HeroBar />
+        <TabMenu />
+        <NavBar />
+
+        <section className="copyright-bar">
+            <p>&copy; Klara van den Burg</p>
+        </section>
+        </>
+    );
+}
+
+export default HomePage;
