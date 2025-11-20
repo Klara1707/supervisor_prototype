caf8f3fff0a5641f572260146c08e4eaa5f24fc5
Klara <klara.vandenburg@yahoo.com>
2025-11-20 08:18:52 +0800

    Remove build folder from tracking

--- build/vite.config.js
+++
@@ -1,8 +0,0 @@
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/supervisor_prototype/', // 👈 Required for GitHub Pages
-    plugins: [react()],
-});
